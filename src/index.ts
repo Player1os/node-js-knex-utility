50bce2f072d8092047dbac56ea0f84579858658d
Osama Hassanein <player1os@gmail.com>
2017-06-05 05:12:35 +0200

    Update types and implementation details

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 import config from '@player1os/config'
 
 // Load npm modules.
-import * as knex from 'knex'
+import * as Knex from 'knex'
 import * as lodash from 'lodash'
 
 // Load node modules.
@@ -11,32 +11,43 @@ import * as path from 'path'
 // Load the project's knex configuration.
 const knexConfig = __non_webpack_require__(path.join(config.APP_ROOT_PATH, 'knexFile.js'))
 
+// Define the helper class.
+// TODO: Use type definition for this and the double column.
+class KnexWrapperHelper {
+	alterColumn(columnBuilder: Knex.ColumnBuilder): Knex.ColumnBuilder {
+		return (columnBuilder as any).alter()
+	}
+}
+
+// Expose an instace of the helper.
+export const helper = new KnexWrapperHelper()
+
 // Expose the knex connection wrapper.
 export default {
-	// Define the is_connected flag.
+	// Define the is connected flag.
 	isConnected: false,
 	// Define the knex instance.
-	instance: knex({
+	instance: Knex({
 		client: knexConfig.client,
 	}),
 	// Define the connection semaphore.
 	semaphore: 0,
-	// Connect to the underlying database through knex.
+	// Connect to the database through knex.
 	async connect() {
 		// Defermine whether a connection has already been established.
 		if (this.semaphore === 0) {
 			// Create the knex instance based on the config.
-			this.instance = knex(knexConfig)
+			this.instance = Knex(knexConfig)
 			this.isConnected = true
 		}
 
 		// Increment semaphore.
 		(++this.semaphore)
 
-		// Return a copy of the connection parameters.
+		// Return the connection parameters.
 		return lodash.clone(knexConfig.connection) || {}
 	},
-	// Disconnect from the underlying database through knex.
+	// Disconnect from the database through knex.
 	async disconnect() {
 		// Determine if the disconnect method was used incorrectly.
 		if (this.semaphore === 0) {
@@ -51,13 +62,22 @@ export default {
 			return
 		}
 
-		// End the connection to the database.
+		// Perform the actual disconnecting.
 		await this.instance.destroy()
 
 		// Reset the knex instance.
-		this.instance = knex({
+		this.instance = Knex({
 			client: knexConfig.client,
 		})
 		this.isConnected = false
+	},
+	outputSqlString(inputs: Knex.QueryBuilder | Knex.QueryBuilder[] | Knex.SchemaBuilder | Knex.SchemaBuilder[]) {
+		const processedInputs = lodash.isArray(inputs)
+			? inputs
+			: [inputs]
+
+		return processedInputs.map((processedInput) => {
+			return processedInput.toString()
+		}).join('\n\n')
 	},
 }
