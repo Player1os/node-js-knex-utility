e4e2aef7e7788497eb2e2e4a5eddcb4d461c1b39
Osama Hassanein <player1os@gmail.com>
2017-06-12 23:35:19 +0200

    Update the library structure to contain a class and named exports

--- src/index.ts
+++ src/index.ts
@@ -2,51 +2,65 @@
 import * as Knex from 'knex'
 import * as lodash from 'lodash'
 
-// Define the is connected flag.
-export let isConnected = false
+// Expose the class for the main knex wrapper.
+export class KnexWrapper {
+	// Define the knex instance.
+	public instance: Knex = null
 
-// Define the knex instance.
-export let instance: Knex | null = null
+	// Define the is connected flag.
+	public isConnected = false
 
-// Define the connection semaphore.
-let semaphore = 0
+	// Define the connection semaphore.
+	private semaphore = 0
 
-// Connect to the database through knex.
-export const connect = async (knexConfig: Knex.Config) => {
-	// Defermine whether a connection has already been established.
-	if (semaphore === 0) {
-		// Create the knex instance based on the config.
-		instance = Knex(knexConfig)
-		isConnected = true
+	// Connect to the database through knex.
+	async connect(knexConfig: Knex.Config) {
+		// Defermine whether a connection has already been established.
+		if (this.semaphore === 0) {
+			// Create the knex instance based on the config.
+			this.instance = Knex(knexConfig)
+			this.isConnected = true
+		}
+
+		// Increment the semaphore.
+		(++this.semaphore)
 	}
 
-	// Increment semaphore.
-	(++semaphore)
+	// Disconnect from the database through knex.
+	async disconnect() {
+		// Determine if the disconnect method was used incorrectly.
+		if (this.semaphore === 0) {
+			throw new Error('Disconnect invoked before connect.')
+		}
+
+		// Decrement the semaphore.
+		(--this.semaphore)
+
+		// Do nothing if the connect method wasn't called a sufficient number of times.
+		if (this.semaphore > 0) {
+			return
+		}
+
+		// Perform the actual disconnecting.
+		await this.instance.destroy()
+
+		// Reset the knex instance.
+		this.instance = null
+		this.isConnected = false
+	}
+
+	async transaction(callback: (trx: Knex.Transaction) => Promise<any>, trx: Knex.Transaction = null) {
+		if (trx) {
+			return callback(trx)
+		}
+
+		if (this.instance) {
+			return this.instance.transaction(callback)
+		}
+	}
 }
 
-// Disconnect from the database through knex.
-export const disconnect = async () => {
-	// Determine if the disconnect method was used incorrectly.
-	if (semaphore === 0) {
-		throw new Error('Disconnect invoked before connect.')
-	}
-
-	// Decrement semaphore.
-	(--semaphore)
-
-	// Do nothing if the connect method wasn't called a sufficient number of times.
-	if (semaphore > 0) {
-		return
-	}
-
-	// Perform the actual disconnecting.
-	await instance.destroy()
-
-	// Reset the knex instance.
-	instance = null
-	isConnected = false
-}
-
+// Define and expose a helper function for outputing query builders into sql strings.
 export const outputSqlString = (inputs: Knex.QueryBuilder | Knex.QueryBuilder[] | Knex.SchemaBuilder | Knex.SchemaBuilder[]) => {
 	const processedInputs = lodash.isArray(inputs)
 		? inputs
@@ -57,13 +71,10 @@ export const outputSqlString = (inputs: Knex.QueryBuilder | Knex.QueryBuilder[] 
 	}).join('\n\n')
 }
 
-// Define the helper class.
-// TODO: Use type definition for this and the double column.
-export class KnexWrapperHelper {
-	alterColumn(columnBuilder: Knex.ColumnBuilder): Knex.ColumnBuilder {
-		return (columnBuilder as any).alter()
-	}
+// Define and expose a helper function for altering columns within a schema.
+export const alterColumn = (columnBuilder: Knex.ColumnBuilder): Knex.ColumnBuilder => {
+	return (columnBuilder as any).alter()
 }
 
-// Expose an instace of the helper.
-export const helper = new KnexWrapperHelper()
+// Expose an instace of the wrapper.
+export default new KnexWrapper()
