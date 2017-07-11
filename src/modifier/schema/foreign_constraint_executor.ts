bd57566ec0f801aa0772556c4e9b5d665b075bf0
Osama Hassanein <osama.hassanein@cellense.com>
2017-07-11 18:23:27 +0200

    Remake into a freaking class

--- src/modifier/schema/foreign_constraint_executor.ts
+++ src/modifier/schema/foreign_constraint_executor.ts
@@ -5,8 +5,15 @@ import * as Knex from 'knex'
  * Execute a collection of knex column builders that already defines a foreign constraint to be defferable.
  * @param knexColumnBuilders A knex column builder defining the foreign constraint to be created.
  */
-export default async (knexInstance: Knex, knexColumnBuilders: Knex.ColumnBuilder[]) => {
-	return Promise.all(knexColumnBuilders.map((knexColumnBuilder) => {
-		return knexInstance.raw(`${knexColumnBuilder.toString()} deferrable initially deferred`)
-	}))
+export default class ForeignKeyExecutor {
+	private _alterQueries: string[] = []
+
+	public add(knexColumnBuilder: Knex.ColumnBuilder) {
+		this._alterQueries.push(`${knexColumnBuilder.toString()} deferrable initially deferred`)
+	}
+
+	async execute(knexInstance: Knex) {
+		await knexInstance.raw(this._alterQueries.join(';'))
+		this._alterQueries = []
+	}
 }
