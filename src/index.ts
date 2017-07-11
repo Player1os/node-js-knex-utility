b1dcc32d60ccb4255e15dba1321185e2d99b269e
Osama Hassanein <osama.hassanein@cellense.com>
2017-07-11 18:24:10 +0200

    DUf

--- src/index.ts
+++ src/index.ts
@@ -51,7 +51,7 @@ export { default as filerExpressionItemQueryModifier } from '.../src/modifier/qu
 
 // Expose the schema modifier functions.
 export { default as alterColumnSchemaModifier } from '.../src/modifier/schema/alter_column'
-export { default as foreignConstraintExecutorSchemaModifier } from '.../src/modifier/schema/foreign_constraint_executor'
+export { default as ForeignConstraintExecutorSchemaModifier } from '.../src/modifier/schema/foreign_constraint_executor'
 
 // Expose the connection class and instance.
 export { Connection }
