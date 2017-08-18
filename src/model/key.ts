7d63770bb42ac169de74b064bed0f7fa40f459bb
Osama Hassanein <osama.hassanein@cellense.com>
2017-08-18 18:36:01 +0200

    Remove entity modify and delete from the key model

--- src/model/key.ts
+++ src/model/key.ts
@@ -6,9 +6,6 @@ import {
 	IModifyOptions,
 } from '.../src/model/base'
 
-// Load npm modules.
-import * as lodash from 'lodash'
-
 /**
  * An abstract base class for entities containing a primary key.
  * This extends the base model class, with additional methods that assume the presence of a primary key in the underlying database.
@@ -67,22 +64,6 @@ export abstract class KeyModel<
 	}
 
 	/**
-	 * Modify the entity indicated by the primary key that's part of the supplied entity, by storing its current field values.
-	 * @param this An instance of the KeyModel class.
-	 * @param entity An entity previously retrieved from the underlying database.
-	 * @param options A set of options that determine how the query is executed and whether the inputs are validated.
-	 * @throws EntityNotFoundError.
-	 */
-	public async modifyEntity(
-		this: KeyModel<TKey, IEntity, ICreateValues, IModifyValues, IFilterItem, IInsertValues, IUpdateValues, IWhereFilterItem>,
-		entity: IEntity,
-		options: IModifyOptions = {},
-	) {
-		// Call the modify by key method, using the entity's primary key and its field values.
-		return this.modifyByKey(entity.key, lodash.pick(entity, lodash.without(this.fieldNames, 'key')) as IModifyValues, options)
-	}
-
-	/**
 	 * Destroy a single entity of the model with a matching primary key.
 	 * @param this An instance of the KeyModel class.
 	 * @param key A value that will be matched against the primary key of all entities in the underlying database.
@@ -96,21 +77,5 @@ export abstract class KeyModel<
 	) {
 		// Call the destroy one method with the primary key in the filter expression.
 		return this.destroyOne({ key } as IFilterItem, options)
-	}
-
-	/**
-	 * Destroy the entity indicated by the primary key that's part of the supplied entity.
-	 * @param this An instance of the KeyModel class.
-	 * @param entity An entity previously retrieved from the underlying database.
-	 * @param options A set of options that determine how the query is executed and whether the inputs are validated.
-	 * @throws EntityNotFoundError.
-	 */
-	public async destroyEntity(
-		this: KeyModel<TKey, IEntity, ICreateValues, IModifyValues, IFilterItem, IInsertValues, IUpdateValues, IWhereFilterItem>,
-		entity: IEntity,
-		options: IDestroyOptions = {},
-	) {
-		// Call the destroy by key method, using the entity's primary key.
-		return this.destroyByKey(entity.key, options)
 	}
 }
